576063692ad31d45b969e4d0dfc2f8a5629fd630
m00nberri <zaberkat@gmail.com>
2022-10-13 21:53:37 +0900

    Create storage object and addBook function

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function Book (title, author, pages, haveRead) {
 Book.prototype.info = function () {
     return `${this.title} by ${this.author}, is ${this.pages} pages long, ${(this.haveRead) ? 'finished' : 'not read yet'}`
 }
+
+const libraryBuilding = {
+    bookShelf: [],
+    addBook: function (title, author, pages, haveRead) {
+        let book = (new Book(title, author, pages, haveRead))
+        this.bookShelf.push(book);
+    }
+}
