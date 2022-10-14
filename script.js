d42aa83e8cc687a82a4bf9092166a8619f9a81a8
m00nberri <aberkat@gmail.com>
2022-10-14 14:00:46 +0900

    Add click eventlistenr to all buttons and clickHandler

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ const libraryBuilding = {
         this.bookShelf.push(book);
     }
 }
+
+const buttons = document.querySelectorAll('button');
+for (const button of buttons) {
+    button.addEventListener('click', (e) => {
+        clickHandler(e);
+    })
+}
+
+function clickHandler(e) {
+    if (e.target.id === 'newBookButton') {
+        modal.style.display = 'grid';
+    }
+    else if (e.target.id === 'closeButton') {
+        modal.style.display = 'none';
+    }
+    else if (e.target.id === 'submitButton') {
+        //do some stuff
+    }
+}
+
+const modal = document.getElementById('newBookForm');
