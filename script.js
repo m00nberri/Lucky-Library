243aa601c5e07f831e3f2ca808a7191af8580d40
m00nberri <aberkat@gmail.com>
2022-10-17 09:19:55 +0900

    Woops I don't remember (WIP push)

--- script.js
+++ script.js
@@ -9,13 +9,58 @@ Book.prototype.info = function () {
     return `${this.title} by ${this.author}, is ${this.pages} pages long, ${(this.haveRead) ? 'finished' : 'not read yet'}`
 }
 
-//Book.prototype.createBookCard = function () {}
+Book.prototype.createBookCard = function () {
+    let bookShelf = document.getElementById('bookShelf')
+
+    let card = document.createElement('div')
+    card.classList.add('bookCard')
+
+    let title = document.createElement('div')
+    title.textContent = this.title;
+    title.id = this.title;
+    card.appendChild(title)
+
+    let author = document.createElement('div')
+    author.textContent = this.author;
+    card.appendChild(author)
+
+    let pages = document.createElement('div')
+    let pagesLabel = document.createElement('span')
+    pages.textContent = this.pages;
+    pagesLabel.textContent = 'pages';
+    card.appendChild(pages)
+
+    let buttonContainer = document.createElement('div')
+    let readLabel = document.createElement('span')
+    readLabel.textContent = 'Read Status:';
+    let readButton = document.createElement('button')
+    buttonContainer.appendChild(readLabel)
+    buttonContainer.appendChild(readButton)
+    let delButton = document.createElement('button')
+    delButton.textContent = 'Delete'
+    buttonContainer.appendChild(delButton)
+    card.appendChild(buttonContainer)
+
+    bookShelf.appendChild(card)
+}
 
 const library = {
     bookShelf: [],
     addBook: function (title, author, pages) {
         let book = (new Book(title, author, pages))
+        book.number = this.bookShelf.length;
         this.bookShelf.push(book);
+    },
+    populateShelf: function () {
+        for (const book of this.bookShelf) {
+            if (book.title === document.getElementById(book.title)) {
+                continue
+            }
+            else {
+                book.createBookCard();
+                alert('pp fart')
+            }
+        }
     }
 }
 
@@ -39,6 +84,7 @@ function clickHandler(e) {
         if (formValidate()) {
             library.addBook(document.forms['formo']['book title'].value,document.forms['formo']['author'].value,document.forms['formo']['number of pages'].value);
             modal.style.display = 'none';
+            library.populateShelf();
         }
         else {
             alert('you dick fuck');
@@ -67,3 +113,15 @@ function clearForm () {
     document.forms['formo']['author'].value = '';
     document.forms['formo']['number of pages'].value = '';
 };
+
+function populateShelf () {
+    for (const book of library.bookShelf) {
+        if (book.title === document.getElementById(book.title)) {
+            continue
+        }
+        else {
+            book.createBookCard;
+            alert('pp fart')
+        }
+    }
+}
